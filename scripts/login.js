a865f5712a5644ac9b74822133b138e561a31bf4
RehabBakhet <bakhetrehab@gmail.com>
2025-07-28 21:07:49 +0300

    add login.css

--- scripts/login.js
+++ scripts/login.js
@@ -3,6 +3,10 @@ const password = document.getElementById("password");
 const loginBtn = document.getElementById("login-btn");
 const togglePasswordIcon = document.getElementById("togglePassword");
 
+function handelUserName(event) {
+	loginBtn.disabled = event.target.value === '' ? true : false
+}
+
 function togglePassword() {
   const isPassword = password.type === "password";
   password.type = isPassword ? "text" : "password";
@@ -34,34 +38,7 @@ function loginCheck() {
   }
 
   loginBtn.disabled = false;
-const loginBtn = document.getElementById("login-btn")
-
-function handelUserName(event) {
-	loginBtn.disabled = event.target.value === '' ? true : false
 }
 
-function togglePassword() {
-	password.type = password.type === 'password' ? 'text' : 'password'
-}
 
-function loginCheck() {
-    loginBtn.disabled = true;
 
-    const usersData = localStorage.getItem("users");
-    const users = usersData ? JSON.parse(usersData) : [];
-
-    const user = users.find( function matchUser(user) { return user.username === username.value; });
-
-	if (user) {
-		if (user.password === password.value) {
-			sessionStorage.setItem("userSession", JSON.stringify(user));
-			alert("Login successful!");
-			console.log(sessionStorage.getItem('userSession'))
-		} else 
-			alert("Sorry, wrong password");
-	} else
-		alert(username.value + " user is not registered yet");
-
-	loginBtn.disabled = false;
-}
-}
